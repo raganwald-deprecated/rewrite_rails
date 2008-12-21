73cb4fc99e78331aab3ef8d22e73a4da7004e33f
Reginald Braithwaite <raganwald@gmail.com>
2008-12-20 23:36:59 -0500

    simple andand specs

--- spec/andand_spec.rb
+++ spec/andand_spec.rb
@@ -19,4 +19,34 @@ describe RewriteRails::Andand do
     @it.process(RewriteRails.clean { 5.bar(5) do; end }).to_a.should == RewriteRails.clean { 5.bar(5) do; end }.to_a
   end
   
+  it "should rewrite an empty method invocation" do
+    @it.process(RewriteRails.clean { foo.andand.bar }).to_a.should == RewriteRails.clean do
+      (__TEMP__ = foo and __TEMP__.bar)
+    end.to_a
+  end
+
+  it "should rewrite a method invocation with a parameter" do
+    @it.process(RewriteRails.clean { foo.andand.bar(5) }).to_a.should == RewriteRails.clean do
+      (__TEMP__ = foo and __TEMP__.bar(5))
+    end.to_a
+  end
+
+  it "should rewrite a method invocation with a block" do
+    @it.process(RewriteRails.clean { foo.andand.bar { |x| x } }).to_a.should == RewriteRails.clean do
+      (__TEMP__ = foo and __TEMP__.bar { |x| x } )
+    end.to_a
+  end
+
+  it "should rewrite a method invocation with a block passed" do
+    @it.process(RewriteRails.clean { foo.andand.bar(&:x) }).to_a.should == RewriteRails.clean do
+      (__TEMP__ = foo and __TEMP__.bar(&:x) )
+    end.to_a
+  end
+
+  it "should rewrite a method invocation with a parameter and a block passed" do
+    @it.process(RewriteRails.clean { foo.andand.bar(5, &:x) }).to_a.should == RewriteRails.clean do
+      (__TEMP__ = foo and __TEMP__.bar(5, &:x) )
+    end.to_a
+  end
+  
 end
