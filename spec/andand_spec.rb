8e04c1cef0987f5ee5a1fc64abd6d9fb3a7785f5
Reginald Braithwaite <raganwald@gmail.com>
2008-12-20 23:55:55 -0500

    pendinged some future ideas

--- spec/andand_spec.rb
+++ spec/andand_spec.rb
@@ -19,34 +19,64 @@ describe RewriteRails::Andand do
     @it.process(RewriteRails.clean { 5.bar(5) do; end }).to_a.should == RewriteRails.clean { 5.bar(5) do; end }.to_a
   end
   
-  it "should rewrite an empty method invocation" do
-    @it.process(RewriteRails.clean { foo.andand.bar }).to_a.should == RewriteRails.clean do
-      (__TEMP__ = foo and __TEMP__.bar)
-    end.to_a
-  end
+  describe "current functionality" do
+  
+    it "should rewrite an empty method invocation" do
+      @it.process(RewriteRails.clean { foo.andand.bar }).to_a.should == RewriteRails.clean do
+        (__TEMP__ = foo and __TEMP__.bar)
+      end.to_a
+    end
 
-  it "should rewrite a method invocation with a parameter" do
-    @it.process(RewriteRails.clean { foo.andand.bar(5) }).to_a.should == RewriteRails.clean do
-      (__TEMP__ = foo and __TEMP__.bar(5))
-    end.to_a
-  end
+    it "should rewrite a method invocation with a parameter" do
+      @it.process(RewriteRails.clean { foo.andand.bar(5) }).to_a.should == RewriteRails.clean do
+        (__TEMP__ = foo and __TEMP__.bar(5))
+      end.to_a
+    end
 
-  it "should rewrite a method invocation with a block" do
-    @it.process(RewriteRails.clean { foo.andand.bar { |x| x } }).to_a.should == RewriteRails.clean do
-      (__TEMP__ = foo and __TEMP__.bar { |x| x } )
-    end.to_a
-  end
+    it "should rewrite a method invocation with a block" do
+      @it.process(RewriteRails.clean { foo.andand.bar { |x| x } }).to_a.should == RewriteRails.clean do
+        (__TEMP__ = foo and __TEMP__.bar { |x| x } )
+      end.to_a
+    end
 
-  it "should rewrite a method invocation with a block passed" do
-    @it.process(RewriteRails.clean { foo.andand.bar(&:x) }).to_a.should == RewriteRails.clean do
-      (__TEMP__ = foo and __TEMP__.bar(&:x) )
-    end.to_a
-  end
+    it "should rewrite a method invocation with a block passed" do
+      @it.process(RewriteRails.clean { foo.andand.bar(&:x) }).to_a.should == RewriteRails.clean do
+        (__TEMP__ = foo and __TEMP__.bar(&:x) )
+      end.to_a
+    end
 
-  it "should rewrite a method invocation with a parameter and a block passed" do
-    @it.process(RewriteRails.clean { foo.andand.bar(5, &:x) }).to_a.should == RewriteRails.clean do
-      (__TEMP__ = foo and __TEMP__.bar(5, &:x) )
-    end.to_a
+    it "should rewrite a method invocation with a parameter and a block passed" do
+      @it.process(RewriteRails.clean { foo.andand.bar(5, &:x) }).to_a.should == RewriteRails.clean do
+        (__TEMP__ = foo and __TEMP__.bar(5, &:x) )
+      end.to_a
+    end
+  
+  end
+  
+  describe "future optimizations" do
+    
+    it "should not create a temporary for a variable lookup" do
+      pending "unimplemented"
+      foo = nil
+      @it.process(RewriteRails.clean { foo.andand.bar }).to_a.should == RewriteRails.clean do
+        (foo and foo.bar)
+      end.to_a
+    end
+    
+    it "should NOOP for a truthy literal" do
+      pending "unimplemented"
+      @it.process(RewriteRails.clean { 42.andand.bar }).to_a.should == RewriteRails.clean do
+        42.bar
+      end.to_a
+    end
+    
+    it "should NOOP for a falsy literal" do
+      pending "unimplemented"
+      @it.process(RewriteRails.clean { nil.andand.bar }).to_a.should == RewriteRails.clean do
+        nil
+      end.to_a
+    end
+    
   end
   
 end
